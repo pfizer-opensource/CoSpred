ddfb69ca2e97bd31ba9fb79297814186430356ef
xuel12 <liang.xue@pfizer.com>
2025-04-21 23:05:02 -0400

    add demo with docker

---
+++ demo/capsule/run.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -ex
+
+pwd
+# clone repo
+# git clone https://github.com/pfizer-opensource/CoSpred.git --depth 1
+git -C CoSpred pull || git clone https://github.com/pfizer-opensource/CoSpred.git CoSpred --depth 1
+
+# copy example data and models to the corresponding directory
+mkdir -p CoSpred/prediction
+mkdir -p CoSpred/data
+unzip -o ../data/pretrained_models.zip -d CoSpred/model_spectra/
+unzip -o ../data/example.zip -d CoSpred/data/
+
+# change workdir to cospred and execute scripts
+cd CoSpred
+
+# --- For the codes below, uncomment sessions based on the needs --- #
+## Split the dataset into train and test set. (about 3 mins)
+# python rawfile2hdf_prosit.py -w split
+
+## Pair database search result with MGF spectrum, reformat to full MSMS using bins. (about 1 min)
+# python rawfile2hdf_cospred.py -w train
+# python rawfile2hdf_cospred.py -w test
+
+## training workflows
+# python training_cospred.py -fc
+
+## spectrum prediction from sequence
+python prediction.py -f
+
+## plotting spectrum
+python spectra_plot.py
+# --- END of main codes --- #
+
+# store result
+mv data ../../results/                  # transformed data
+mv model_spectra ../../results/         # trained models
+mv prediction ../../results/            # prediction results
